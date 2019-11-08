e909a4c3d86d5dfeafe3c2e4430766d036ac00a6
lifu <lifu@tuzhanai.com>
2019-11-08 10:06:54 +0800

    添加immutable-tool

--- Demo/react/t-react/src/components/tool-space/components/immutable/index.tsx
+++ Demo/react/t-react/src/components/tool-space/components/immutable/index.tsx
@@ -1,11 +1,32 @@
 import React from 'react';
-
+import Immutable from 'immutable'
 type IProps = {};
 
 type IState = {};
 
+
+const data = {
+  box:'nice',
+  person:{
+    age:29,
+    name:'richlee'
+  }
+}
+
+class Rdata extends Immutable.Record(data){
+
+}
+
+
 export default class App extends React.Component<IProps, IState> {
+  state = new Rdata()
+
+  componentDidMount(){
+
+  }
   render() {
-    return <div>immutable</div>;
+    console.log(this.state);
+    // const name = this.state.getIn(['person','name']);
+  return <div>111</div>;
   }
 }
