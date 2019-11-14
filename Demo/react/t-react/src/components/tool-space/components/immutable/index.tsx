1e1c6f7696813d205793080c3c2db3ed5bc637b7
lifu <lifu@tuzhanai.com>
2019-11-14 10:47:58 +0800

    添加nginx模块笔记

--- Demo/react/t-react/src/components/tool-space/components/immutable/index.tsx
+++ Demo/react/t-react/src/components/tool-space/components/immutable/index.tsx
@@ -27,6 +27,21 @@ export default class App extends React.Component<IProps, IState> {
   render() {
     console.log(this.state);
     // const name = this.state.getIn(['person','name']);
-  return <div>111</div>;
+  return <div>
+    <svg
+          className="ring-box"
+          width={(5 + 10) * 2}
+          height={(5 + 10) * 2}>
+          <circle
+            r={5}
+            cx={10}
+            cy={10}
+            fill="none"
+            stroke={'#cccccc'}
+            stroke-width={10}
+          />
+          </svg>
+    <div data-chart="nice"></div>
+  </div>;
   }
 }
